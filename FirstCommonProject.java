9d4510220480f919e1ba0e7d629e4f018afedd86
FranivVolodya <franiv_v@meta.ua>
2015-05-20 21:57:12 +0300

    Volodya first commit

---
+++ FirstCommonProject.java
@@ -0,0 +1,13 @@
+/**
+*@author Volodya Franiv
+*/
+public class FirstCommonProject{
+
+	static void volodya (){
+		System.out.println("Hi Volodya");
+	} 
+	
+	public static void main(String...args){
+		volodya();
+	} 
+}
